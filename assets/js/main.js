017a23e9376b2d45c2707a9f0d9b402ec89adc47
Justine <103948035+Justine3237@users.noreply.github.com>
2023-05-12 22:20:49 +0530

    removing popup main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,11 +96,3 @@
   new WOW().init();
 })();
 
-document.querySelector("#butt").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "block";
-});
-
-
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-});
