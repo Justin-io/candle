1a7b38a6dea9a9bc180664b54c2e0d0d0e6bfb2e
Justine <103948035+Justine3237@users.noreply.github.com>
2023-05-25 09:23:46 +0530

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,1 +1,17 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $feedback = $_POST['feedback'];
 
+    // Here, you can perform additional processing or validation with the submitted data
+
+    // Save the data to a file or database
+    $data = "Name: $name\nPhone: $phone\nfeedback: $feedback\n";
+    file_put_contents('form_data.txt', $data, FILE_APPEND | LOCK_EX);
+
+    // Redirect the user to a thank-you page or display a success message
+    header('Location: thank_you.html');
+    exit();
+}
+?>
